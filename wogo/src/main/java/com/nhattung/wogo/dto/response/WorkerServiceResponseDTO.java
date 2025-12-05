5ba0cbde9d33dd92eaf54ffc5d6255279e0c6d9b
ngonhattung <ngonhattung08062003@gmail.com>
2025-12-05 21:34:34 +0700

    fix dto

--- wogo/src/main/java/com/nhattung/wogo/dto/response/WorkerServiceResponseDTO.java
+++ wogo/src/main/java/com/nhattung/wogo/dto/response/WorkerServiceResponseDTO.java
@@ -1,12 +1,16 @@
 package com.nhattung.wogo.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class WorkerServiceResponseDTO {
 
     private Long id;
